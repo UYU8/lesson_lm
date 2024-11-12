1c011ba517aae961f0c00daf62d0354d5d6d3cb0
UYU8 <1262864216@qq.com>
2024-11-12 21:31:32 +0800

    LLM

--- JS/scope/1.js
+++ JS/scope/1.js
@@ -1,16 +1,3 @@
 var a = 1;
-var b = 4;
-function foo(){
-    console.log(a,b);
 
-    var a = 2;
-    // console.log(a);
-    var a = 3
-    function bar(){
-        return a + b;
-    }   
-    // console.log(bar());
-
-    // console.log(a,b);
-}
-foo();
+a();
