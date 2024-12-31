f026b742b30a22c8c006e0287a95e7a0af4ea493
UYU8 <1262864216@qq.com>
2024-12-31 19:50:44 +0800

    组件设计

--- vue_react/reactive/1.js
+++ vue_react/reactive/1.js
@@ -1,6 +1,7 @@
 // es6 代理对象
 const proxy = new Proxy({}, {
-    // 拦截行为
+    // 拦截行为 尝试访问对象时触发get 改变或设置时触发set 
+    // obj即target对象 prop就是time属性 value就是35
     get: function(obj,prop){
         console.log('设置get 操作');
         return obj [prop]
