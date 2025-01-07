371d6112b41a0c212a6a5911908a25adb104f95a
UYU8 <1262864216@qq.com>
2025-01-07 21:25:25 +0800

    vue-hooks

---
+++ vue_react/vue-hooks/src/components/MousePos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <p>Mouse X:{{ x }}</p>
+      <p>Mouse X:{{ y }}</p>
+    </div>
+</template>
+
+<script setup>
+import { useMouse, useMemo } from "../hooks/useMouse";
+import Hook from "../hooks/useMouse";
+
+const { x, y } = useMouse();
+useMemo();
+
+console.log(Hook);
+
+</script>
+
+<style scoped>
+
+</style>
